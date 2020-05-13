e284f276a46757f32334eb9f10283146f2f3e7df
Зиннур <rz@certit.ru>
2020-05-13 22:16:02 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Libl
+{
+    class Program
+    {
+        static class Lib1
+        {
+           public static string stpow(string ha)
+            {
+               string x = ha + ha + ha + ha;
+                return x;
+            }
+            public static string rev(string a)
+            {
+                char[] y = a.ToCharArray();
+                Array.Reverse(y); // реверсировать строку и сформировать новую.
+                return new string(y);
+            }
+            public static string  remov(string str, string s)
+            {
+                Console.Write("Введите подстроку: ");
+                s = Console.ReadLine();
+                str = str.Replace(s, "");
+                return str;
+            }
+            public static string leng(string str)
+            {
+                int z = str.Length;
+                string c = z.ToString();
+                return c;
+            }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
